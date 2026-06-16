c0da0028cae64e84c267346d23432a6eb3c9b9eb
yesblue0342-bit <yesblue0342@gmail.com>
2026-06-16 17:28:27 +0900

    feat: Service Worker (캐시 + 오프라인)

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Stella Clover Service Worker
+const CACHE = 'stella-clover-v1';
+
+self.addEventListener('install', e => self.skipWaiting());
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// API 요청은 캐시 안 함, 나머지는 캐시
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  if (url.pathname.startsWith('/api/')) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
